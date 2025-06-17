07c3dc1e5c54186dd65b2ecc9e031210ef791d7c
Miradji-Ladhidhou <ladhidhoum@gmail.com>
2025-06-17 20:24:35 +0400

    style form auth

--- views/pages/auth.php
+++ views/pages/auth.php
@@ -1,9 +1,9 @@
 <h2>Connexion</h2>
 
-<form action="index.php?page=loginProcess" method="POST">
+<form action="index.php?page=loginProcess" method="POST" class="auth">
     <input type="email" name="email" placeholder="Email" required><br>
     <input type="password" name="password" placeholder="Mot de passe" required><br>
-    <button type="submit">Se connecter</button>
+    <button type="submit" class="btn btn-success">Se connecter</button>
 </form>
 
 <?php if (!empty($error)): ?>
